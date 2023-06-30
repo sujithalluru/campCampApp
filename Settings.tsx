a9ca845935cbd36f5a8b3d5334592cdf857b418b
CodingPro445 <slowbird1108@gmail.com>
2023-06-30 10:28:14 -0500

    account deletion

--- Settings.tsx
+++ Settings.tsx
@@ -4,6 +4,8 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
+import auth from '@react-native-firebase/auth';
+
 
 type RootStackParamList = {
   Login: undefined;
@@ -37,11 +39,34 @@ const Settings = ({ navigation }: SettingsScreenProps) => {
     
   };
 
+  const handleDeletePress = async () => {
+    await AsyncStorage.setItem('isLoggedIn', '0');
+    await AsyncStorage.setItem('role', '');
+    await AsyncStorage.setItem('code', '');
+    await AsyncStorage.setItem("volTime", '');
+    await AsyncStorage.setItem("installTime", '');
+    messaging()
+    .unsubscribeFromTopic('volunteer')
+    messaging()
+    .unsubscribeFromTopic('all')
+    await auth().currentUser?.delete();
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Login' }],  // use the name of your home screen here
+    });
+    
+    
+  };
+
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.logoutButton} onPress={handleLogoutPress}>
         <Icon name="sign-out" size={24} color="#fff" />
         <Text style={styles.logoutButtonText}>Log Out</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.logoutButton} onPress={handleDeletePress}>
+        <Icon name="delete" size={24} color="#fff" />
+        <Text style={styles.logoutButtonText}>Delete Account</Text>
       </TouchableOpacity>
     </View>
   );
